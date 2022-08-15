08b2d9e6477e913e8053be28e3ee4c6657a50cc8
Bobooshodi <feyi.tobi@gmail.com>
2022-08-16 04:47:56 +0800

    WIP: imageListControl

---
+++ client/src/components/controls/FileUploadFieldset.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-upload
+    v-model:file-list="files"
+    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+    list-type="picture-card"
+    :on-preview="handlePictureCardPreview"
+    :on-remove="handleRemove"
+  >
+    <el-icon>
+      <Plus />
+    </el-icon>
+  </el-upload>
+
+  <el-dialog v-model="dialogVisible">
+    <img w-full :src="dialogImageUrl" alt="Preview Image" />
+  </el-dialog>
+</template>
+<script>
+export default {
+  name: 'FileUploadFieldset',
+  props: {
+    files: { typea: Array },
+  },
+};
+</script>
+<style lang=""></style>
